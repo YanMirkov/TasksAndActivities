1efe06e5c84243731e328d5979d8f70c7c754940
user <user@users-Mac-mini-3.local>
2015-02-01 19:39:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/user/tasksandactivities/Activity3.java
@@ -0,0 +1,15 @@
+package com.example.user.tasksandactivities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by user on 2/1/15.
+ */
+public class Activity3 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+    }
+}
---
+++ app/src/main/java/com/example/user/tasksandactivities/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.user.tasksandactivities;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Activity2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
